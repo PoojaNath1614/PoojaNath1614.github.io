4ca31be62f84fc8667a1bf4803698e8faf4db675
Pooja Nath <poojanath2002@gmail.com>
2024-05-14 11:30:42 +0530

    basic css

---
+++ index.js
@@ -0,0 +1,9 @@
+const colorchangebtn=document.getElementById("colorchangebtn");
+const dreamhomesbtn=document.getElementById("dreamhomesbtn");
+
+colorchangebtn.addEventListener('click',()=>{
+
+});
+dreamhomesbtn.addEventListener('click',()=>{
+
+});
